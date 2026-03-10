89e741e7fc8240ae5ee6090d2049fef214503c34
yakuzaofficialprp-cell <yakuzaofficialprp@gmail.com>
2026-03-10 16:11:36 +0500

    index.js

--- index.js
+++ index.js
@@ -250,8 +250,9 @@ client.on('guildMemberAdd', async member => {
         .setTitle("Tec Trader")
         .setDescription(
             `Hey <@${member.id}>, welcome to **Tec Trader**!\n\n` +
-            `YOU WILL FIND CONTENT RELATED TO ALL\n` +
-            `GAMES AND SOFTWARES.`
+            `https://discord.com/channels/1337111106971504661/1344666865057923193\n` +
+            `https://discord.com/channels/1337111106971504661/1397437073841197127.`
+            `https://discord.com/channels/1337111106971504661/1337266092812406844.`
         )
         .setThumbnail(client.user.displayAvatarURL({ forceStatic: true }))
         .setImage("https://cdn.discordapp.com/attachments/1337788828051701873/1480098172075376743/standard_1.gif?ex=69b06a97&is=69af1917&hm=3893d590b6f33d4d6baf945e5674c7b47e4803eefed8b70db4cf51a95f3b7907&")
